7df102dd26f4b802a633a0b4029d85f48a12a901
Rushabh Jain <rushabhjain452@gmail.com>
2024-02-15 00:55:55 +0530

    added option to delete attendance for sabha with less than 10 members

--- display_datewise_number_of_members.php
+++ display_datewise_number_of_members.php
@@ -16,6 +16,20 @@
     $row = $result->fetch_assoc();
     $numberOfMembers = $row['number_of_members'];
 
+    // Delete Attendance
+    if(isset($_GET['deleteDate'])) {
+        $deleteDate = $_GET['deleteDate'];
+        $sql = "DELETE FROM attendance WHERE date='$deleteDate'";
+        if($con->query($sql)){
+            echo '<script> alert("Attendance deleted successfully"); </script>';
+        }else{
+            echo $sql."<br>";
+            echo '<script> alert("Something went wrong..."); </script>';
+            // echo "<h3>Something went wrong...!</h3>";
+        }
+    }
+
+    // Get Attendance
     $startDate = isset($_GET['startDate']) ? $_GET['startDate'] : null;
     $endDate = isset($_GET['endDate']) ? $_GET['endDate'] : null;
 
@@ -116,6 +130,17 @@
 							<td class="text-center">
                                 <?= DateTime::createFromFormat('Y-m-d', $row['date'])->format('d M Y') ?> 
                                 (<?= DateTime::createFromFormat('Y-m-d', $row['date'])->format('l') ?>)
+                                <?php
+                                    if($row['number_of_members'] < 10) {
+                                ?>
+                                    <a href="display_datewise_number_of_members.php?deleteDate=<?=$row['date']?>&startDate=<?=$startDate?>&endDate=<?=$endDate?>" class="btn btn-sm btn-danger" title="Delete">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+                                        </svg>
+                                    </a>
+                                <?php
+                                    }
+                                ?>
                             </td>
 							<td class="text-center"> <?= $row['number_of_members'] ?> </td>
                             <td class="text-center"> <?= $per ?> % </td>
